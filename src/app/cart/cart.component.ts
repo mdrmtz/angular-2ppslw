741f1c489db515b93e92636838d5761546648c57
mdrmtz <mdrmtz@gmail.com>
2021-05-24 15:33:24 -0700

    Create the checkout form.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -19,7 +19,7 @@ export class CartComponent implements OnInit {
     private formBuilder: FormBuilder
   ) {}
 
-onSubmit(): void {
+  onSubmit(): void {
     // Process checkout data here
     this.items = this.cartService.clearCart();
     console.warn('Your order has been submitted', this.checkoutForm.value);
